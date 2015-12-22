0e256559e62caa8a9ab3aa62f2e453fde5e09e1e
lymslive <403708621@qq.com>
2015-12-22 21:16:45 +0800

    opt func

--- fun/gendkeylist.m
+++ fun/gendkeylist.m
@@ -1,16 +1,24 @@
 function dkey = gendkeylist(kmain, ksub)
 % 生成双键列表 2015-12-11
-% kmain: 主键个数
-% ksub: 次键个数
+% kmain: 主键个数，或直接的主键向量，若是标量，则生成 1:kmain
+% ksub: 次键个数，或直接的次键向量，若是标量，则生成 1:ksub
 % dkey: 返回两列矩阵
 %
 
-n = kmain * ksub;
+if numel(kmain) == 1
+    kmain = (1 : kmain);
+end
+
+if numel(ksub) == 1
+    ksub = (1 : ksub);
+end
+
+n = numel(kmain) * numel(ksub);
 dkey = zeros(n, 2);
 
 n = 0;
-for s = 1 : ksub
-    for m = 1 : kmain
+for m = kmain
+    for s = ksub
         n = n + 1;
         dkey(n, 1) = m;
         dkey(n, 2) = s;
